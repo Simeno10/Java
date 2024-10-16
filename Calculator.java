568a28f742ead2f7c31ce260c9591f9a00ad2723
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2024-10-16 22:07:17 +0200

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+import static org.junit.Assert.assertEquals;
+
+public class Calculator {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę.");
+        int wiek = scanner.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Podaj drugą.");
+        int x = scanner.nextInt();
+        System.out.println("Wynik dodawania to: "+(wiek+x));
+        System.out.println("Wynik odejmowania to: "+(wiek-x));
+        System.out.println("Wynik mnożenia to: "+(wiek*x));
+        System.out.println("Wynik dzielenia to: "+(wiek/x));
+
+
+}
+}

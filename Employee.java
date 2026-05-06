3e4b94224efbc08bcffc4e115b2a152dde360d3a
Szymon Janiszewski <58219271+Simeno10@users.noreply.github.com>
2026-05-06 12:48:10 +0200

    Refactor Employee class with JPA annotations

--- Employee.java
+++ Employee.java
@@ -1,44 +1,138 @@
-package com.barosanu;
+package com.barosanu.empl.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "employees")
+@Entity
 public class Employee {
 
-    private String name;
-    private int age;
-    private int salary;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date hireDate;
+    private String position;
 
-    public Employee(String name, int age, int salary) {
-        this.name = name;
-        this.age = age;
-        this.salary = salary;
+    public Employee() {
     }
 
-    // Getters and Setters
-    public String getName() {
-        return name;
+
+    public Employee(String firstName, String lastName, Date hireDate, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hireDate = hireDate;
+        this.position = position;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Long getId() {
+        return id;
     }
 
-    public int getAge() {
-        return age;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public int getSalary() {
-        return salary;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
+    public String getLastName() {
+        return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((hireDate == null) ? 0 : hireDate.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (hireDate == null) {
+            if (other.hireDate != null)
+                return false;
+        } else if (!hireDate.equals(other.hireDate))
+            return false;
+        if (position == null) {
+            if (other.position != null)
+                return false;
+        } else if (!position.equals(other.position))
+            return false;
+        return true;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", hireDate=" + hireDate
+                + ", position=" + position + "]";
+    }
 
     
+
+    
+
     
     
 }
